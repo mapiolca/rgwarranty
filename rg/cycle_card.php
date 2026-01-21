12b8e4456751478788bdd04078d6cc5b8630d951
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-01-21 22:34:00 +0100

    Update cycle_card.php

--- rg/cycle_card.php
+++ rg/cycle_card.php
@@ -354,7 +354,7 @@ if ($action != 'presend') {
 	print '<div class="fichehalfleft">';
 	// EN: Documents block
 	// FR: Bloc documents
-	$modulepart = 'rgwarranty';
+	$modulepart = 'rgw_cycle';
 	// EN: Align output directory with generateDocument()
 	// FR: Aligner le répertoire de sortie avec generateDocument()
 	$documentref = dol_sanitizeFileName($object->ref);
