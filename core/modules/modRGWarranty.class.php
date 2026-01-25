08983b59e1d7393a0ed560558b5ad47bc0dd5f77
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-01-25 22:43:49 +0100

    Fix menu key format in modRGWarranty class

--- core/modules/modRGWarranty.class.php
+++ core/modules/modRGWarranty.class.php
@@ -139,7 +139,7 @@ if (!class_exists('modRGWarranty')) {
 		);
 		*/
 		$this->menu[$r++] = array(
-			'fk_menu' => 'fk_mainmenu=billing&fk_leftmenu=customers_bills',
+			'fk_menu' => 'fk_mainmenu=billing,fk_leftmenu=customers_bills',
 			'type' => 'left',
 			'titre' => 'RGWMenuRetention',
 			//'prefix' => img_picto('', $this->picto, 'class="paddingright pictofixedwidth valignmiddle"'),
