bc17f631cc9d7704187cea3c604d7d3a66a3d42f
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-02-12 23:48:11 +0100

    Update cycle_card.php

--- rg/cycle_card.php
+++ rg/cycle_card.php
@@ -490,6 +490,7 @@ if ($action == 'presend') {
 	$defaulttopic = 'RGWRequestLetterTitle';
 	$objref = dol_sanitizeFileName($object->ref);
 	$diroutput = getMultidirOutput($object).'/'.$object->element;
+	var_dump($diroutput);
 	$trackid = 'rgwarranty'.$object->id;
 	include DOL_DOCUMENT_ROOT.'/core/tpl/card_presend.tpl.php';
 }
